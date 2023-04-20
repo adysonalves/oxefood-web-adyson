f02d76595f060a3a465e08845b6bb08fea878149
= <=>
2023-04-20 20:35:39 -0300

    Enviado dados para api

--- src/views/cliente/FormCliente.jsx
+++ src/views/cliente/FormCliente.jsx
@@ -1,22 +1,51 @@
+import axios from "axios";
 import React from "react";
 import InputMask from 'react-input-mask';
 import { Button, Container, Divider, Form, Icon } from 'semantic-ui-react';
 
-class FormCliente extends React.Component{
+class FormCliente extends React.Component {
+	state = {
 
-    render(){
-        return(
-            <div>
+		nome: null,
+		cpf: null,
+		dataNascimento: null,
+		foneCelular: null,
+		foneFixo: null
+	}
 
-                <div style={{marginTop: '3%'}}>
+	salvar = () => {
 
-                    <Container textAlign='justified' >
+		let clienteRequest = {
 
-                        <h2> <span style={{color: 'darkgray'}}> Cliente &nbsp;<Icon name='angle double right' size="small" /> </span> Cadastro </h2>
+			nome: this.state.nome,
+			cpf: this.state.cpf,
+			dataNascimento: this.state.dataNascimento,
+			foneCelular: this.state.foneCelular,
+			foneFixo: this.state.foneFixo
+		}
+	
+		axios.post("http://localhost:8082/api/cliente", clienteRequest)
+		.then((response) => {
+			console.log('Cliente cadastrado com sucesso.')
+		})
+		.catch((error) => {
+			console.log('Erro ao incluir o um cliente.')
+		})
+	}
 
-                        <Divider />
+	render() {
+		return (
+			<div>
 
-						<div style={{marginTop: '4%'}}>
+				<div style={{ marginTop: '3%' }}>
+
+					<Container textAlign='justified' >
+
+						<h2> <span style={{ color: 'darkgray' }}> Cliente &nbsp;<Icon name='angle double right' size="small" /> </span> Cadastro </h2>
+
+						<Divider />
+
+						<div style={{ marginTop: '4%' }}>
 
 							<Form>
 
@@ -27,50 +56,64 @@ class FormCliente extends React.Component{
 										fluid
 										label='Nome'
 										maxLength="100"
+										value={this.state.nome}
+										onChange={e => this.setState({ nome: e.target.value })}
 									/>
 
 									<Form.Input
 										fluid
-										label='CPF'>
-										<InputMask 
-										mask="999.999.999-99"/> 
+										label='CPF'
+										value={this.state.cpf}
+										onChange={e => this.setState({ cpf: e.target.value })}
+									>
+										<InputMask
+											mask="999.999.999-99" />
+
 									</Form.Input>
 
 								</Form.Group>
-								
+
 								<Form.Group>
 
 									<Form.Input
 										fluid
 										label='Fone Celular'
-                                        width={6}>
-										<InputMask 
-										mask="(99) 9999.9999" /> 
+										width={6}
+										value={this.state.foneCelular}
+										onChange={e => this.setState({ foneCelular: e.target.value })}
+									>
+										<InputMask
+											mask="(99) 9999.9999" />
 									</Form.Input>
 
 									<Form.Input
 										fluid
 										label='Fone Fixo'
-                                        width={6}>
-										<InputMask 
-										mask="(99) 9999.9999" /> 
+										width={6}
+										value={this.state.foneFixo}
+										onChange={e => this.setState({ foneFixo: e.target.value })}
+									>
+										<InputMask
+											mask="(99) 9999.9999" />
 									</Form.Input>
 
-                                    <Form.Input
-                                        fluid
-                                        label='Data Nascimento'
-                                        width={6}
-                                    >
-                                        <InputMask 
-                                            mask="99/99/9999" 
-                                            maskChar={null}
-                                            placeholder="Ex: 20/03/1985"
-                                        /> 
-                                    </Form.Input>
+									<Form.Input
+										fluid
+										label='Data Nascimento'
+										width={6}
+									>
+										<InputMask
+											mask="99/99/9999"
+											maskChar={null}
+											placeholder="Ex: 20/03/1985"
+											value={this.state.dataNascimento}
+											onChange={e => this.setState({ dataNascimento: e.target.value })}
+										/>
+									</Form.Input>
 
 								</Form.Group>
 
-								<Form.Group widths='equal' style={{marginTop: '4%'}}  className='form--empresa-salvar'>
+								<Form.Group widths='equal' style={{ marginTop: '4%' }} className='form--empresa-salvar'>
 
 									<Button
 										type="button"
@@ -80,13 +123,13 @@ class FormCliente extends React.Component{
 										labelPosition='left'
 										color='orange'
 										onClick={this.listar}
-										>
+									>
 										<Icon name='reply' />
 										Voltar
 									</Button>
 
 									<Container textAlign='right'>
-										
+
 										<Button
 											inverted
 											circular
@@ -99,15 +142,15 @@ class FormCliente extends React.Component{
 											<Icon name='save' />
 											Salvar
 										</Button>
-										
+
 									</Container>
 
 								</Form.Group>
 
 							</Form>
 						</div>
-                    </Container>
-                </div>
+					</Container>
+				</div>
 			</div>
 		)
 	}
