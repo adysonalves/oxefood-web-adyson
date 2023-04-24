0239dcb99f5343696b7593b3db807e9e4bca19fd
= <=>
2023-04-24 19:32:08 -0300

    Implementação do axios

--- src/views/entregador/FormEntregador.jsx
+++ src/views/entregador/FormEntregador.jsx
@@ -1,133 +1,253 @@
+import axios from 'axios';
 import React from "react";
 import InputMask from 'react-input-mask';
-import { Button, Container, Divider, Form, Icon, TextArea } from 'semantic-ui-react';
+import { Button, Container, Divider, Form, Icon } from 'semantic-ui-react';
 
 class FormEntregador extends React.Component {
+    state = {
+
+		nome: null,
+		cpf: null,
+        rg: null,
+		dataNascimento: null,
+		foneCelular: null,
+		foneFixo: null,
+        qtdEntregasRealizadas: null,
+        valorFrete: null,
+        enderecoRua: null,
+        enderecoNumero: null,
+        enderecoBairro: null,
+        enderecoCidade: null,
+        enderecoCep: null,
+        enderecoUf: null,
+        enderecoComplemento: null,
+        ativo: false
+
+	}
+
+
+	salvar = () => {
+
+		let EntregadorRequest = {
+
+			nome: this.state.nome,
+			cpf: this.state.cpf,
+            rg: this.state.rg,
+			dataNascimento: this.state.dataNascimento,
+			foneCelular: this.state.foneCelular,
+			foneFixo: this.state.foneFixo,
+            qtdEntregasRealizadas: this.state.qtdEntregasRealizadas,
+            valorFrete: this.state.valorFrete,
+            enderecoRua: this.state.enderecoRua,
+            enderecoNumero: this.state.enderecoNumero,
+            enderecoBairro: this.state.enderecoBairro,
+            enderecoCidade: this.state.enderecoCidade,
+            enderecoCep: this.state.enderecoCep,
+            enderecoCep: this.state.enderecoCep,
+            enderecoUf: this.state.enderecoUf,
+            enderecoComplemento: this.state.enderecoComplemento,
+            ativo: this.state.ativo
+		}
+	
+		axios.post("http://localhost:8082/api/cliente", EntregadorRequest)
+		.then((response) => {
+			console.log('Entregador cadastrado com sucesso.')
+		})
+		.catch((error) => {
+			console.log('Erro ao incluir o um cliente.')
+		})
+	}
 
     render() {
         return (
             <div>
 
-                <div style={{ marginTop: '3%' }}>
+            <div style={{marginTop: '3%'}}>
 
-                    <Container textAlign='justified' >
+                <Container textAlign='justified' >
 
-                        <h2> <span style={{ color: 'darkgray' }}> Entregador &nbsp;<Icon name='angle double right' size="small" /> </span> Cadastro </h2>
+                    <h2> <span style={{color: 'darkgray'}}> Entregador &nbsp;<Icon name='angle double right' size="small" /> </span> Cadastro </h2>
 
-                        <Divider />
+                    <Divider />
 
-                        <div style={{ marginTop: '4%' }}>
+                    <div style={{marginTop: '4%'}}>
 
-                            <Form>
+                        <Form>
 
-                                <Form.Group widths='equal'>
+                            <Form.Group>
 
-                                    <Form.Input
-                                        required
-                                        fluid
-                                        label='Nome'
-                                        maxLength="100"
-                                    />
+                                <Form.Input
+                                    fluid
+                                    label='Nome'
+                                    width={8}
+                                    required
+                                />
 
-                                    <Form.Input
-                                        required
-                                        fluid
-                                        label='CPF'>
-                                        <InputMask
-                                            mask="999.999.999-99" />
+                                <Form.Input
+                                    fluid
+                                    label='CPF'
+                                    required
+                                    width={4}>
+                                        <InputMask 
+                                            mask="999.999.999-99"/>
                                     </Form.Input>
 
-                                    <Form.Input
-                                        fluid
-                                        label='RG'>
+                                <Form.Input
+                                    fluid
+                                    label='RG'
+                                    width={4}
+                                />
+
+                            </Form.Group>
+
+                            <Form.Group>
+
+                                <Form.Input
+                                    fluid
+                                    label='DT Nascimento'
+                                    width={3}>
+                                        <InputMask mask="99/99/9999" placeholder="Ex: 20/03/1985"/>
+                                </Form.Input>
+                                
+
+                                <Form.Input
+                                    fluid
+                                    label='Fone Celular'
+                                    required
+                                    width={4}>
+                                        <InputMask mask="(99) 99999.9999"/>
+                                </Form.Input>
+                                
+
+                                <Form.Input
+                                    fluid
+                                    label='Fone Fixo'
+                                    width={4}>
+                                        <InputMask mask="(99) 9999.9999"/>
                                     </Form.Input>
+                                
 
-                                </Form.Group>
+                                <Form.Input
+                                    fluid
+                                    label='QTD Entregas Realizadas'
+                                    width={3}
+                                />
 
+                                <Form.Input
+                                    fluid
+                                    label='Valor Por Frete'
+                                    width={3}
+                                />
+                                
+                            </Form.Group>
+
+                            <Form.Group>
+
+                                <Form.Input
+                                    fluid
+                                    label='Rua'
+                                    width={13}
+                                />
+
+                                <Form.Input
+                                    fluid
+                                    label='Número'
+                                    width={3}
+                                />
+
+                            </Form.Group>
 
                                 <Form.Group>
 
-                                    <Form.Input
-                                        fluid
-                                        label='DT Nascimento'
-                                        
-                                    >
-                                        <InputMask
-                                            mask="99/99/9999" />
-                                    </Form.Input>
-
-                                </Form.Group>
-
-
-
-                                <TextArea
-                                    label='Descrição'
-                                    placeholder='Informe a descrição do produto'
-
+                                <Form.Input
+                                    fluid
+                                    label='Bairro'
+                                    width={7}
                                 />
 
-                                <div style={{ marginBottom: '1%' }}></div>
+                                <Form.Input
+                                    fluid
+                                    label='Cidade'
+                                    width={7}
+                                />
 
-                                <Form.Group widths='equal' >
-
-                                    <Form.Input
-                                        required
-                                        fluid
-                                        label='Valor Unitário'
-                                        maxLength="100"
-                                    />
-
-                                    <Form.Input
-                                        fluid
-                                        label='Tempo de Entrega Mínimo em Minutos'>
+                                <Form.Input
+                                    fluid
+                                    label='CEP'
+                                    width={2}>
+                                        <InputMask mask="99.999-9999"/>
                                     </Form.Input>
+                                
 
-                                    <Form.Input
-                                        fluid
-                                        label='Tempo de Entrega Máximo em Minutos'>
-                                    </Form.Input>
+                            </Form.Group>
+                            
+                            <Form.Input
+                                fluid
+                                label='UF'
+                            />
 
-                                </Form.Group>
 
-                                <Form.Group widths='equal' style={{ marginTop: '4%' }} className='form--empresa-salvar'>
+                            <Form.Input
+                                fluid
+                                label='Complemento'
+                            />
 
+                            <Form.Group inline>
+
+                                <label>Ativo: </label>
+
+                                <Form.Radio
+                                    label='Sim'
+                                    value='Sim'
+                                    checked={true}
+                                />
+                                
+                                <Form.Radio
+                                    label='Não'
+                                    value='Não'
+                                />
+
+                            </Form.Group>
+                            
+                            <Form.Group widths='equal' style={{marginTop: '4%'}}  className='form--empresa-salvar'>
+
+                                <Button
+                                    type="button"
+                                    inverted
+                                    circular
+                                    icon
+                                    labelPosition='left'
+                                    color='orange'
+                                    onClick={this.listar}
+                                    >
+                                    <Icon name='reply' />
+                                    Voltar
+                                </Button>
+
+                                <Container textAlign='right'>
+                                    
                                     <Button
-                                        type="button"
                                         inverted
                                         circular
                                         icon
                                         labelPosition='left'
-                                        color='orange'
-                                        onClick={this.listar}
+                                        color='blue'
+                                        floated='right'
+                                        onClick={this.salvar}
                                     >
-                                        <Icon name='reply' />
-                                        Voltar
+                                        <Icon name='save' />
+                                        Salvar
                                     </Button>
+                                    
+                                </Container>
 
-                                    <Container textAlign='right'>
+                            </Form.Group>
 
-                                        <Button
-                                            inverted
-                                            circular
-                                            icon
-                                            labelPosition='left'
-                                            color='blue'
-                                            floated='right'
-                                            onClick={this.salvar}
-                                        >
-                                            <Icon name='save' />
-                                            Salvar
-                                        </Button>
-
-                                    </Container>
-
-                                </Form.Group>
-
-                            </Form>
-                        </div>
-                    </Container>
-                </div>
+                        </Form>
+                    </div>
+                </Container>
             </div>
+        </div>
         )
     }
 }
--- src/views/produto/FormProduto.jsx
+++ src/views/produto/FormProduto.jsx
@@ -1,7 +1,39 @@
+import axios from 'axios';
 import React from "react";
 import { Button, Container, Divider, Form, Icon, TextArea } from 'semantic-ui-react';
 
 class FormCliente extends React.Component {
+
+    state = {
+
+        codigo: null,
+        titulo: null,
+        descricao: null,
+        valorUnitario: null,
+        tempoEntregaMinimo: null,
+        tempoEntregaMaximo: null
+    }
+
+    salvar = () => {
+
+        let ProdutoRequest = {
+
+            codigo: this.state.codigo,
+            titulo: this.state.titulo,
+            descricao: this.state.descricao,
+            valorUnitario: this.state.valorUnitario,
+            tempoEntregaMinimo: this.state.tempoEntregaMinimo,
+            tempoEntregaMaximo: this.state.tempoEntregaMaximo
+        }
+
+        axios.post("http://localhost:8082/api/produto", ProdutoRequest)
+            .then((response) => {
+                console.log('Produto cadastrado com sucesso.')
+            })
+            .catch((error) => {
+                console.log('Erro ao incluir o um Produto.')
+            })
+    }
 
     render() {
         return (
@@ -26,12 +58,18 @@ class FormCliente extends React.Component {
                                         fluid
                                         label='Título'
                                         maxLength="100"
+                                        value={this.state.titulo}
+                                        onChange={e => this.setState({ titulo: e.target.value })}
                                     />
 
                                     <Form.Input
                                         required
                                         fluid
-                                        label='Código do Produto'>
+                                        label='Código do Produto'
+                                        value={this.state.codigo}
+                                        onChange={e => this.setState({ codigo: e.target.value })}
+
+                                    >
                                     </Form.Input>
 
                                 </Form.Group>
@@ -41,10 +79,12 @@ class FormCliente extends React.Component {
                                 <TextArea
                                     label='Descrição'
                                     placeholder='Informe a descrição do produto'
-                                    
+                                    value={this.state.descricao}
+                                    onChange={e => this.setState({ descricao: e.target.value })}
+
                                 />
 
-                                <div style={{marginBottom: '1%'}}></div>
+                                <div style={{ marginBottom: '1%' }}></div>
 
                                 <Form.Group widths='equal' >
 
@@ -53,16 +93,24 @@ class FormCliente extends React.Component {
                                         fluid
                                         label='Valor Unitário'
                                         maxLength="100"
+                                        value={this.state.valorUnitario}
+                                        onChange={e => this.setState({ valorUnitario: e.target.value })}
                                     />
 
                                     <Form.Input
                                         fluid
-                                        label='Tempo de Entrega Mínimo em Minutos'>
+                                        label='Tempo de Entrega Mínimo em Minutos'
+                                        value={this.state.tempoEntregaMinimo}
+                                        onChange={e => this.setState({ tempoEntregaMinimo: e.target.value })}
+                                        >
                                     </Form.Input>
 
                                     <Form.Input
                                         fluid
-                                        label='Tempo de Entrega Máximo em Minutos'>
+                                        label='Tempo de Entrega Máximo em Minutos'
+                                        value={this.state.tempoEntregaMaximo}
+                                        onChange={e => this.setState({ tempoEntregaMaximo: e.target.value })}
+                                        >
                                     </Form.Input>
 
                                 </Form.Group>
