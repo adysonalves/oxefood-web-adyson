b92f6af2d1f602cda62033994f74c5ed3045f699
= <=>
2023-05-15 19:42:03 -0300

    Material

--- src/Rotas.jsx
+++ src/Rotas.jsx
@@ -3,11 +3,13 @@ import { Route, Routes } from "react-router-dom";
 
 import FormCliente from './views/cliente/FormCliente';
 import ListCliente from './views/cliente/ListCliente';
-import ListEntregador from './views/entregador/ListEntregador';
 import FormEntregador from './views/entregador/FormEntregador';
+import ListEntregador from './views/entregador/ListEntregador';
+import Home from './views/home/home';
+import FormMaterial from './views/material/FormMaterial';
 import FormProduto from './views/produto/FormProduto';
 import ListProduto from './views/produto/ListProduto';
-import Home from './views/home/home';
+
 
 function Rotas() {
     return (
@@ -20,6 +22,8 @@ function Rotas() {
                 <Route path="form-produto" element={ <FormProduto/> } />
                 <Route path="list-produto" element={ <ListProduto/> } />
                 <Route path="form-entregador" element={ <FormEntregador/> } />
+                <Route path="form-material" element={ <FormMaterial/> } />
+                
             </Routes>
         </>
     )
